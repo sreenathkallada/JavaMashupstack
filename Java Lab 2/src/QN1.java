d7c80fa26ff972e7cb15cb4f2d47d39ce242eca4
Sreenath S <kalladasreenath@live.com>
2021-10-20 12:46:46 +0530

    lab1 completed

--- Java Lab 2/src/QN1.java
+++ Java Lab 2/src/QN1.java
@@ -1,9 +1,27 @@
-
+import java.util.Scanner;
 public class QN1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x ,y ,z;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		x = input.nextInt();
+		System.out.println("Enter the second number: ");
+		y = input.nextInt();
+		System.out.println("Enter the third number: ");
+		z = input.nextInt();
+		
+		if (x>y && x>z) {
+			System.out.println("The largest number is "+x);
+		}
+		else if(y>z) {
+			System.out.println("The largest number is "+y);
+		}
+		else {
+			System.out.println("The largest number is "+z);
+		}
+		input.close();
+		
 	}
-
 }
