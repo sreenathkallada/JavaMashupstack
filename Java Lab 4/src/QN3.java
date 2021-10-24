961418c9b6687643c69a08a5407268ab0e72f760
Sreenath S <kalladasreenath@live.com>
2021-10-24 19:49:29 +0530

    done

---
+++ Java Lab 4/src/QN3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class QN3 {
+
+	public static void main(String[] args) {
+		// Used type casting only
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the decimal number to round: ");
+		float number = input.nextFloat();
+		int numbermod = (int)number;
+		System.out.print("Enter the decimal places to round :");
+		int decimal = input.nextInt();
+		float s = number-numbermod;
+		float newdecimal=-1;
+		
+		for (int i=0;i<decimal;i++) {
+			s = s*10;
+			}
+		int sum = (int)s;
+		float sum1=(float)sum;
+//		System.out.println(sum+ "sum");
+//		System.out.println(sum1+"sum1");
+		for (int i=0;i<decimal;i++) {
+			
+			newdecimal = sum1/10;
+			sum1 = newdecimal;
+			
+		}
+		System.out.println(numbermod+newdecimal);
+		input.close();
+	}
+
+}
--- Java Lab 4/src/QN4.java
+++ Java Lab 4/src/QN4.java
@@ -1,32 +1,41 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class QN4 {
 
 	public static void main(String[] args) {
-		// Used type casting only
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the decimal number to round: ");
-		float number = input.nextFloat();
-		int numbermod = (int)number;
-		System.out.print("Enter the decimal places to round :");
-		int decimal = input.nextInt();
-		float s = number-numbermod;
-		float newdecimal=-1;
-		
-		for (int i=0;i<decimal;i++) {
-			s = s*10;
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter the operwtion(1 for ChartoString and 2 for Stringtochar): ");
+	try{
+	int operation = input.nextInt();
+	
+		if(operation ==2) {
+			System.out.print("Enter the string to convert: ");
+			String string = input.next();
+			char[] toChar = string.toCharArray();
+			for (int i=0; i<toChar.length;i++) {
+				System.out.print(toChar[i]);
 			}
-		int sum = (int)s;
-		float sum1=(float)sum;
-//		System.out.println(sum+ "sum");
-//		System.out.println(sum1+"sum1");
-		for (int i=0;i<decimal;i++) {
-			
-			newdecimal = sum1/10;
-			sum1 = newdecimal;
-			
+//			System.out.println(Arrays.toString(toChar));
 		}
-		System.out.println(numbermod+newdecimal);
-		input.close();
+		else if(operation ==1) {
+			System.out.print("Enter the char to convert: ");
+			char[] toString= new char[10];
+			for(int i=0;i<10;i++){
+				toString[i]= input.next().charAt(0);
+			}
+			String str1 = String.valueOf(toString);
+			String str11 = new String(str1);
+			
+			System.out.print("Char to string is " +str11);
+		}
+		else {
+				System.out.print("Enter a valid operation(1/2)");
+		}
+	}
+	catch(Exception e){
+		System.out.println("Something went wrong, pls check inputs" +e);
+	}
+		
 	}
 
 }
